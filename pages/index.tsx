271ef48aac31e35a8a34153760875177992ded16
ies0411 <ies041196@gmail.com>
2023-01-26 09:36:44 +0900

    first

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+  // console.log('home');
+  // console.log(results);
+  return (
+    <div>
+      <h1>test</h1>
+    </div>
+  );
+}
+
+// // import Router from 'next/dist/server/router';
+
+// //TODO : bootstrap
---
+++ pages/intrinsic/index.tsx
@@ -0,0 +1,7 @@
+export default function Intrinsic() {
+  return (
+    <div>
+      <h1>Intrinsic</h1>
+    </div>
+  );
+}
